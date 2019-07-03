5931e6229698d33607d583a8233cc7b51f2730cd
zgprunty <kprunty@kooomo.com>
2019-07-03 11:08:27 +0100

    more UI tweaks on the story items

--- src/components/StoryListItem.js
+++ src/components/StoryListItem.js
@@ -10,13 +10,17 @@ class StoryListItem extends Component
         const upvote_link = `vote?${story.id}=&how=up&goto=news`;
         const upvote_id = `up_${story.id}`;
 
-        return (<li className="story-item">
-                <a href={upvote_link} id={upvote_id} className="upvote">up vote</a> &nbsp;
-                <a href={story.url}>{story.title}</a> &nbsp;
-                <a href="from?{story.url}">(site)</a> &nbsp;
-                {story.score} points by <a href="user?id={story.by}">{story.by}</a>  &nbsp;| &nbsp;
-                <a href="">hide</a>  &nbsp;| &nbsp;
-                <a href="">{story.descendants} comments</a> 
+        return (
+            <li className="story-item">
+                <div class="flex">
+                    <a href={upvote_link} id={upvote_id} className="upvote">up vote</a>
+                    <a href={story.url}>{story.title}</a>
+                    <a href="from?{story.url}">(site)</a>
+                    <p>
+                        {story.score} points by <a href="user?id={story.by}">{story.by}</a>  &nbsp;| &nbsp;
+                        <a href="">hide</a>&nbsp;| &nbsp; <a href="">{story.descendants} comments</a>
+                    </p>
+                </div>    
             </li>)
     }
 }
